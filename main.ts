53eb153fb0c002596442a209cd3d8e93ad8f6f88
Ivy <61100378+lighthousedino@users.noreply.github.com>
2024-06-02 18:05:29 +0200

    Add command to update modified time

--- main.ts
+++ main.ts
@@ -1,89 +1,118 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Plugin,
+	TFile,
+	moment,
+	MarkdownView,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface UpdateModifiedTimeSettings {
+	debug: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: UpdateModifiedTimeSettings = {
+	debug: false,
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class UpdateModifiedTimePlugin extends Plugin {
+	settings: UpdateModifiedTimeSettings;
 
 	async onload() {
+		console.log("Loading Update Modified Time plugin");
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add a command to manually update the modified time
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "update-modified-time",
+			name: "Update Modified Time",
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.updateModifiedTime(markdownView.file);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
+				return false;
+			},
+		});
+
+		// Add settings tab
+		this.addSettingTab(new UpdateModifiedTimeSettingTab(this.app, this));
+	}
+
+	async updateModifiedTime(file: TFile) {
+		if (this.settings.debug)
+			console.log(`Updating modified time for file: ${file.path}`);
+
+		if (file.extension !== "md") {
+			if (this.settings.debug)
+				console.log("File is not a markdown file, skipping.");
+			return;
+		}
+
+		try {
+			const fileContent = await this.app.vault.read(file);
+			if (this.settings.debug)
+				console.log("File content read successfully");
+			const yamlRegex = /^---\n([\s\S]*?)\n---/;
+			const match = fileContent.match(yamlRegex);
+			const currentTime = moment().utc().format("YYYY-MM-DDTHH:mm:ss[Z]");
+
+			if (!match) {
+				if (this.settings.debug)
+					console.log("No YAML front matter found, skipping update.");
+				return;
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			const yamlContent = match[1];
+			let newYamlContent;
+			if (yamlContent.includes("modified:")) {
+				if (this.settings.debug)
+					console.log("Existing modified field found, updating it");
+				newYamlContent = yamlContent.replace(
+					/modified: .*/,
+					`modified: ${currentTime}`
+				);
+			} else {
+				if (this.settings.debug)
+					console.log("No modified field found, adding it");
+				newYamlContent = yamlContent + `\nmodified: ${currentTime}`;
+			}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			const newFileContent = fileContent.replace(
+				yamlRegex,
+				`---\n${newYamlContent}\n---`
+			);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			if (this.settings.debug) {
+				console.log("New file content prepared");
+				// console.log("Old File Content:", fileContent);
+				// console.log("New File Content:", newFileContent);
+			}
+
+			await this.app.vault.modify(file, newFileContent);
+			if (this.settings.debug)
+				console.log("File content updated and saved");
+		} catch (error) {
+			if (this.settings.debug)
+				console.error("Error updating file", error);
+		}
 	}
 
 	onunload() {
-
+		console.log("Unloading Update Modified Time plugin");
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +120,29 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class UpdateModifiedTimeSettingTab extends PluginSettingTab {
+	plugin: UpdateModifiedTimePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: UpdateModifiedTimePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Debug Mode")
+			.setDesc("Enable debug mode to display detailed logs")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.debug)
+					.onChange(async (value) => {
+						this.plugin.settings.debug = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
